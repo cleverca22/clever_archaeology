454d2c1c81eeb640b7aa31022c4a4c93b2f88e07
clever <clever@2088d4bf-9a48-0410-9deb-f4985f36c06e>
2010-12-20 14:40:31 +0000

    added X and circle image/code
    git-svn-id: svn://thep4/clever_archaelogy@689 2088d4bf-9a48-0410-9deb-f4985f36c06e

--- kashikoi.lua
+++ kashikoi.lua
@@ -6,17 +6,76 @@ local function dump_table(t)
 end
 local surveys, last_survey = {},{}
 local survey_count = 0
+local search_area_width,search_area_height = 20,20
+local search_frames = {}
+local asking = false
+local function get_search_frame(x,y)
+	if search_frames[x] == nil then
+		search_frames[x] = {}
+	end
+	if search_frames[x][y] == nil then
+		local spot = CreateFrame("Frame", nil, nil)
+		spot:SetWidth(15)
+		spot:SetHeight(15)
+		local t = spot:CreateTexture(nil,"BACKGROUND")
+		local foo = [[Interface\AddOns\clever_archaelogy\X.tga]]
+		--local foo = "Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp"
+		t:SetTexture(foo)
+		t:SetAllPoints(spot)
+		spot.texture = t
+		search_frames[x][y] = spot
+	end
+	--search_frames[x][y]:Show()
+	return search_frames[x][y]
+end
+local rings = {}
+local function get_ring(x,y)
+	if rings[x] == nil then
+		rings[x] = {}
+	end
+	if rings[x][y] == nil then
+		local t = kashi_env.main_frame:CreateTexture(nil,"BACKGROUND")
+		local circle = [[Interface\AddOns\clever_archaelogy\circle.tga]]
+		t:SetTexture(circle)
+		t:SetWidth(200)
+		t:SetHeight(200)
+		t:SetPoint("CENTER",0,0)
+		rings[x][y] = t
+	end
+	return rings[x][y]
+end
+local function hide_search_frames()
+	local x,y
+	for x = 0,search_area_width do
+		for y = 0,search_area_height do
+			local spot = get_search_frame(x,y)
+			if spot.placed then
+					result = Astrolabe:RemoveIconFromMinimap( search_frames[x][y] )
+					search_frames[x][y].placed = false
+			end
+			search_frames[x][y]:Hide()
+		end
+	end
+end
 local function clear_binds()
+	asking = false
 	local t = kashi_env.main_frame
 	SetOverrideBinding(t,false,"1")
 	SetOverrideBinding(t,false,"2")
 	SetOverrideBinding(t,false,"3")
 end
 local function reset_surveys()
+	for key,value in pairs(surveys) do
+		if value.texture then
+			value.texture:Hide()
+		end
+	end
 	surveys = {}
 	last_survey = {}
 	survey_count = 0
 	clear_binds()
+	hide_search_frames()
+	survey_count = 0
 end
 local function distance(a,b)
 	--local x = a.x - b.x
@@ -25,11 +84,11 @@ local function distance(a,b)
 	c1,z1 = GetCurrentMapAreaID(), GetCurrentMapDungeonLevel()
 	local dist,xdelta,ydelta = Astrolabe:ComputeDistance(c1,z1,a.x,a.y, c1,z1,b.x,b.y)
 	--print(a.x.." "..a.y.." "..b.x.." "..b.y.." "..(dist*10).." "..xdelta.." "..ydelta)
-	return dist
+	return dist,xdelta,ydelta
 	--return math.sqrt(x*x + y*y)
 end
 local min_dist = {  5, 40,  80 }
-local max_dist = { 41, 78, 330 }
+local max_dist = { 41, 78, 608 }
 local widths = { 25,50,100 }
 local function frame_update()
 	local self,text = {},{}
@@ -43,7 +102,7 @@ local function frame_update()
 			return
 		end
 		local dist = distance(self,value)
-		line = dist.." "..value.answer
+		line = math.floor(dist).." "..value.answer
 		if min_dist[value.answer] > dist then
 			line = line .. " too close to survey"
 			bad = bad + 1
@@ -66,6 +125,70 @@ local function frame_update()
 	end
 	table.insert(text,"good/bad: "..good.."/"..bad)
 	kashi_env.fs:SetText(table.concat(text,"\n"))
+
+	for key,value in pairs(surveys) do
+		local dist,xdelta,ydelta = distance(self,value)
+		for key,ring in pairs(value.rings) do
+			ring:SetPoint("CENTER",xdelta,ydelta)
+		end
+	end
+end
+local function update_all_icons()
+	hide_search_frames()
+	local spots = 1
+	local self = {}
+	local x,y
+	local c1,z1 = GetCurrentMapAreaID(), GetCurrentMapDungeonLevel()
+	self.x,self.y = GetPlayerMapPosition("player")
+	for x = 0,search_area_width do
+		for y = 0,search_area_height do
+			local spot = get_search_frame(x,y)
+			spot.OK = true
+			--spot:Hide()
+		end
+	end
+	local offset = 0.003
+	for x = 0,search_area_width do
+		for y = 0,search_area_height do
+			local spot2 = {}
+			spot2.x,spot2.y = self.x + ((x-(search_area_width/2))*offset),self.y + ((y-(search_area_height/2))*offset)
+			for key,value in pairs(surveys) do
+				local OK = true
+				local min,max = min_dist[value.answer],max_dist[value.answer]
+				local dist = distance(spot2,value)
+				if min > dist then
+					OK = false
+				elseif max < dist then
+					OK = false
+				else
+					OK = true
+				end
+				local spot = get_search_frame(x,y)
+				if OK == false then
+					spot.OK = false
+					if spot.placed then
+						result = Astrolabe:RemoveIconFromMinimap( spot )
+						spot.placed = false
+					end
+					spot:Hide()
+				end
+				spots = spots + 1
+			end
+		end
+	end
+	for x = 0,search_area_width do
+		for y = 0,search_area_height do
+			local spot = get_search_frame(x,y)
+			local spot2 = {}
+			spot2.x,spot2.y = self.x + ((x-(search_area_width/2))*offset),self.y + ((y-(search_area_height/2))*offset)
+			if spot.OK then
+				result = Astrolabe:PlaceIconOnMinimap( spot, c1, z1, spot2.x,spot2.y )
+				spot:Show()
+				spot.placed = true
+			end
+		end
+	end
+	print("did "..spots.." spots")
 end
 local update_hooked = false
 local function hook_update()
@@ -81,15 +204,30 @@ local function unhook_update()
 	end
 end
 local textures = {}
+local function show_new_rings(red,green,blue,answer,survey_count)
+	local rings = {}
+	for dist1,count in pairs(kashi_data.dists[answer]) do
+		local ring = get_ring(survey_count,dist1)
+		ring:SetWidth(dist1/2)
+		ring:SetHeight(dist1/2)
+		ring:SetVertextColor(red,green,blue,count/kashi_data.max_count[answer])
+		ring:Show()
+		table.insert(rings,ring)
+	end
+	return rings
+end
 function CA_answer(answer)
 	clear_binds()
+	if not asking then
+		print("something else went wrong")
+		return
+	end
 	if last_survey.x == nil then
 		print('something went wrong?')
 		return
 	end
 	print(answer)
 	last_survey.answer = answer
-	table.insert(surveys,last_survey)
 	PlaySoundFile("Interface\\AddOns\\TomTom\\Media\\ping.mp3")
 	if textures[survey_count] == nil then
 		textures[survey_count] = kashi_env.main_frame:CreateTexture(nil,"ARTWORK")
@@ -111,10 +249,13 @@ function CA_answer(answer)
 	last_survey.texture:SetPoint("TOP",kashi_env.main_frame,10,-13*survey_count)
 	last_survey.texture:SetSize(50,10)
 	last_survey.texture:SetTexture(red,green,blue)
+	last_survey.rings = show_new_rings(red,green,blue,answer,survey_count)
 	survey_count = survey_count + 1
 	
 	hook_update()
+	table.insert(surveys,last_survey)
 	last_survey = {}
+	update_all_icons()
 end
 local function interact_lag()
 	local npc_name = GameTooltipLeftText1:GetText()
@@ -134,6 +275,18 @@ local function sanitize_data()
 		kashi_data.dists[2] = {}
 		kashi_data.dists[3] = {}
 	end
+	kashi_data.max_count = {}
+	for key1,value1 in pairs(kashi_data.dists) do
+		local max = 0
+		for dist,count in pairs(value1) do
+			print(dist)
+			print(count)
+			if count > max then
+				max = count
+			end
+		end
+		kashi_data.max_count[key1] = max
+	end
 end
 local function post_process(new_entry)
 	for key,value in pairs(new_entry.surveys) do
@@ -142,10 +295,12 @@ local function post_process(new_entry)
 		end
 		value.dist = distance(new_entry,value)
 		local rounded = math.floor(value.dist)
-		if kashi_data.dists[value.answer][rounded] == nil then
-			kashi_data.dists[value.answer][rounded] = 1
-		else
-			kashi_data.dists[value.answer][rounded] = kashi_data.dists[value.answer][rounded] + 1
+		if rounded then
+			if kashi_data.dists[value.answer][rounded] == nil then
+				kashi_data.dists[value.answer][rounded] = 1
+			else
+				kashi_data.dists[value.answer][rounded] = kashi_data.dists[value.answer][rounded] + 1
+			end
 		end
 	end
 	table.sort(kashi_data.dists[1])
@@ -156,7 +311,9 @@ local function handle_spell_finished(self,event,...)
 	local caster,spell_name,arg3,arg4,spellid = ...
 	if spellid == 80451 and caster == "player" then
 		myprint('surveying')
+		self:Show()
 		if not InCombatLockdown() then
+			asking = true
 			SetOverrideBinding(self,false,"1","CA_1")
 			SetOverrideBinding(self,false,"2","CA_2")
 			SetOverrideBinding(self,false,"3","CA_3")
@@ -203,22 +360,22 @@ local function eventHandler(self,event,...)
 			new_entry.zone = GetZoneText()
 			new_entry.subzone = GetSubZoneText()
 			new_entry.surveys = surveys
-			sanitize_data()
-			post_process(new_entry)
+			--sanitize_data()
 			unhook_update()
-			survey_count = 0
+			reset_surveys()
+			post_process(new_entry)
 			kashi_env.fs:SetText("item found")
 			for key,value in pairs(surveys) do
 				value.texture:Hide()
 				value.texture = nil
 			end
-			reset_surveys()
 
 			if kashi_data.zones[new_entry.zone] == nil then
 				kashi_data.zones[new_entry.zone] = {}
 			end
 			table.insert(kashi_data.zones[new_entry.zone],new_entry)
 			--myprint("logged creation of '"..clean_name.."'")
+			self:Hide()
 		end
 	elseif event == "ADDON_LOADED" then
 		if arg1 == "clever_archaelogy" then
@@ -289,6 +446,10 @@ local function kashi_init()
 	--frame:RegisterEvent("GET_ITEM_INFO_RECEIVED")
 	--frame:RegisterEvent("CHAT_MSG_ADDON")
 	frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	frame:RegisterEvent("PLAYER_ENTER_COMBAT") -- might not work
+	frame:RegisterEvent("PLAYER_LEAVE_COMBAT")
+	frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+	frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 	frame:SetMovable(true)
 	--frame:SetClampedToScreen(true)
 	frame:SetScript("OnDragStart", function(frame)
